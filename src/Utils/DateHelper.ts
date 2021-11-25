dec61a58f6fb6c0228681547489fb62079309e10
brunoocastro <bruno.scastro2012@hotmail.com>
2021-11-24 23:28:39 -0300

    feat: Creating Acc Creator Helper + Add Friend Accounts

--- src/Utils/DateHelper.ts
+++ src/Utils/DateHelper.ts
@@ -1,5 +1,5 @@
 import moment, { Moment } from "moment";
-// moment.locale('pt-br')
+moment.locale('pt-br')
 export const momentFormat = "[[]HH:mm:ss - DD/MM/YY[]]";
 export default function getMomentString(date: string = null): string {
   const dateMoment = date ? moment(date) : moment();
--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,7 @@ import getAccValue from "./Utils/GetAccValue";
 import cron from "node-cron";
 import FlowProvider from "./Providers/Platform";
 import getMomentString from "./Utils/DateHelper";
-
-const tonelive = {
-  email: "bruno.scastro2012@hotmail.com",
-  username: "tonelive",
-  refreshToken:
-    "AFxQ4_oPtCFHUNXE8sPZY0PwgY1H25fiWNvre1cnzbDE45O5vOyUwFTZzo_iL_I_c1aLtmhVcNxTSz0v6qrJoNuYEPmMM6qhYgpqbzJXTkfhpYjzQheoV1oino5b0ljr1XWS5scea_yyzsoz1X3NwpXItWIXWUhrcPl7M_kf7DAGhke8lDehKXpzigXqnf4m0gtJhhWnmRm1ezEAaJq7x9j_2zRodf_vqQ",
-};
-
-const eduardolima100 = {
-  email: "eduardolima100@gmail.com",
-  username: "eduardolima100",
-  refreshToken:
-    "AFxQ4_qxgFYDgu6n-0mf4-IYjUH4psG7gVXo_zpzbD-QX6DNLE-7I_qyYsLLK3eR3WmadjL-DzAw-1w5GOXotN7qqvp9AY3i_EUtKOq2LuaZfkKBB8XRMyuoiEZFLoeDsNM7gawPnFQhqza5TAhsXqiyi8rLlKKL3juwyYkTVtug6F8BMT-bytOs_Yo17QtB8WUFmCSNTCozEq-qUar60XX9OVFlfAfVeA",
-};
-
-const brunoocastro = {
-  email: "bruno.c0310@gmail.com",
-  username: "brunoocastro",
-  refreshToken:
-    "AFxQ4_o-0ODio-J_6FcF-w6YyLsNvSLb4l4gHhUxD3bjWsRI_k7lhNK6zz_UbTEpoF8kuiQ1Oz2Pg5XN6FIebDgD6UQCNDA6steZQnutPhfJezpFQnhU9IjCP_6Ydt5SaNzyIArSu8If552RHUx7b61_2Zks2Y42xIoMUU8WxoXfqLlvO3mbwILygk4gIXO9WPPijfnpF1IMcBa_I1EXEjKui-egDSlEew",
-};
-const newstovideo = {
-  email: "newstovideo@gmail.com",
-  username: "newstovideo",
-  refreshToken:
-    "AFxQ4_p2fS4wOpH347u5H67dS_Zxe3kALZArFTdiGlvSnqr6qOy8goUSVtjSdnGAOE3ZVVH3RQIC50HFf0HIrf6BUr4T6tSjaOHZMPj4ZKYf9Sx3J4tEC10Fdj-XVvIUmdhnlxsfsz4o0etLUgORmPNse1K6BheY3g9zwqpZdKI5hP-0cgV9PjUV_whxEfCsNwku3ejG-mpOxndcjgwAFjIEKjWVymHNfQ",
-};
-const newstovideorobot = {
-  email: "newstovideorobot@gmail.com",
-  username: "newstovideorobot",
-  refreshToken:
-    "AFxQ4_pcvz10rbTMbDZkHZ_jvOBRIMRViCuX0tlXNrQeItz69Ln0e7a4P5pQW1zq_msre01eGRgxXDInq5FnPaxq0G_540jR5KYl4-QzAFOTS6Otu8auwQwQ3gy7_audHJ19iNIVKPJ7ndFWHcl4kg0GZCjtLxH3LoYchAlUPDC4vcVcVfak3meuDQHTRhz7Fv7Kc_9VWGnfOksuqJERIb6eHQtQIUtgew",
-};
+import AccountCreator from "./Utils/AccountCreator";
 
 const pickBadgesForAllAccounts = async (badges: string[]) => {
   const accounts = await Repositories.AccountsRepository.getAll();
@@ -88,9 +56,9 @@ const searchForNewBadges = async () => {
 
     const newBadges = await filterNewBadges(uniqueBadges);
     console.log(
-      `${getMomentString()} - Foram encontradas ${
+      `\n${getMomentString()} - Foram encontradas ${
         newBadges.length
-      } novas badges. \nLista de badges: ${newBadges}`
+      } novas badges. \nLista de badges: ${newBadges}\n`
     );
 
     await pickBadgesForAllAccounts(newBadges);
@@ -101,40 +69,6 @@ const searchForNewBadges = async () => {
   }
 };
 
-const createAccounts = async () => {
-  const tone = new Account(
-    tonelive.email,
-    tonelive.username,
-    tonelive.refreshToken
-  );
-  const lima = new Account(
-    eduardolima100.email,
-    eduardolima100.username,
-    eduardolima100.refreshToken
-  );
-  const bruno = new Account(
-    brunoocastro.email,
-    brunoocastro.username,
-    brunoocastro.refreshToken
-  );
-  const ntv = new Account(
-    newstovideo.email,
-    newstovideo.username,
-    newstovideo.refreshToken
-  );
-  const ntvr = new Account(
-    newstovideorobot.email,
-    newstovideorobot.username,
-    newstovideorobot.refreshToken
-  );
-
-  await Repositories.AccountsRepository.set(tone);
-  await Repositories.AccountsRepository.set(lima);
-  await Repositories.AccountsRepository.set(bruno);
-  await Repositories.AccountsRepository.set(ntv);
-  await Repositories.AccountsRepository.set(ntvr);
-};
-
 const TwitterInstance = new TwitterProvider(searchForNewBadges);
 const PlatformInstance = new FlowProvider();
 
@@ -143,7 +77,7 @@ const run = async () => {
   try {
     // await getAccValue('dimi_');
     // await getAccValue('gui_aguiar_');
-    await createAccounts();
+    await AccountCreator();
     await searchForNewBadges();
   } catch (e) {
     console.error(e);
